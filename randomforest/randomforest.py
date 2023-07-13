a149d2fe7560e6d546c7056f32527044b538fd9d
Wylie Borden <61087703+PanangCurry@users.noreply.github.com>
2023-07-13 12:45:08 -0600

    Add files via upload

---
+++ randomforest/randomforest.py
@@ -0,0 +1,22 @@
+#this file performs random forest on the processed dataframe containing all features
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error as mse
+from sklearn.metrics import r2_score
+
+#load df, assign X and y, then split with sklearn:
+df = pd.read_csv('df_processed.csv', encoding='ISO-8859-1')
+y = df["rating"]
+X = df.drop(["rating"], axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.10, random_state=24)
+
+#fit rf and predict rating:
+rf = RandomForestRegressor(n_estimators=200, random_state=12)
+rf.fit(X_train, y_train)
+y_pred = rf.predict(X_test)
+
+err = mse(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print(r2)
+
