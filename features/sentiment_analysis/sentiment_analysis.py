e1141f01d6969de4e010dce2136b52393ebef7e5
Danielle Angelini <deangelini@wpi.edu>
2023-07-16 17:54:07 -0400

    fixup! sentiment analysis and new dataframe
    fixup

--- features/sentiment_analysis/sentiment_analysis.py
+++ features/sentiment_analysis/sentiment_analysis.py
@@ -4,9 +4,6 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 mcd_file = "CleanedData/cleaned_reviews.csv"
 
-nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download('stopwords')
 nltk.download('vader_lexicon')
 nltk.download('omw-1.4')
 
