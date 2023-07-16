6ff501570379bb63c3c4cbdb99d2366295edd221
Danielle Angelini <deangelini@wpi.edu>
2023-07-16 17:39:19 -0400

    sentiment analysis and new dataframe

--- features/sentiment_analysis/sentiment_analysis.py
+++ features/sentiment_analysis/sentiment_analysis.py
@@ -1,70 +1,47 @@
 import pandas as pd
 import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from nltk.stem import WordNetLemmatizer
-from textblob import TextBlob 
 
-mcd_file = "cleaned_reviews.csv"
+mcd_file = "CleanedData/cleaned_reviews.csv"
 
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('stopwords')
 nltk.download('vader_lexicon')
-
-stop_words = set(stopwords.words('english'))
-stop_words.add('wa')
-stop_words.add('get')
-lemmatizer = WordNetLemmatizer()
-
-def preprocess_text(text):
-    tokens = word_tokenize(text.lower())
-    tokens = [lemmatizer.lemmatize(token) for token in tokens if token.isalpha()]
-    tokens = [token for token in tokens if token not in stop_words]
-    return tokens
-
-def getPolarity(text):
-   return TextBlob(text).sentiment.polarity
+nltk.download('omw-1.4')
 
 def sentiment_analysis(input_file):
     #get dataset
-    df = pd.read_csv(input_file, encoding='ISO-8859-1')
+    df = pd.read_csv(input_file, encoding="latin-1")
     reviews = df["review"]
     
-    #preprocess reviews
-    preprocessed_rvws = []
-    for i, review in reviews.iteritems():
-        tokens = preprocess_text(review)
-        preprocessed_rvws.append(tokens)
+    #preprocess ratings
+    df['rating'] = df['rating'].str.replace(' star', '')
+    df['rating'] = df['rating'].str.replace('s', '')
+    df['rating'] = df['rating'].astype(int)
+    ratings = df['rating']
 
     sia = SentimentIntensityAnalyzer()
     compound_scores = []
-    polarity_scores = []
+    idx = 0
     
     #calculating polarity score
-    for review in preprocessed_rvws:
-        #calculating polarity score
-        polarity_score = review.apply(getPolarity)
-        polarity_scores.append(polarity_score)
-        
-        #calculating compound score    
-        compound_score = sia.polarity_scores(review)['compound']
-        compound_scores.append(compound_score)
-            
-    #calculating "super score"
-    num_reviews = len(preprocessed_rvws)
-    super_scores = []
-    
-    for i in range(num_reviews):
-        super_scores[i] = polarity_score[i] * compound_score[i]
+    for review in reviews:
+        #calculating compound score 
+        if type(review) != str:
+            rating_value = ratings[idx]
+            compound_score = 2*((rating_value - 1) /4) - 1
+            compound_scores.append(compound_score)
+        else:
+            compound_score = sia.polarity_scores(review)['compound']
+            compound_scores.append(compound_score)
+        idx += 1
         
     #create new dataset
-    sentiment_df = pd.DataFrame(list(zip(polarity_scores, compound_scores, super_scores)), 
-                          columns = ['polarity_score', 'compound_score', 'super_score'])
+    sentiment_df = pd.DataFrame(list(zip(compound_scores)), columns = ['compound_score'])
     new_df = pd.concat([df, sentiment_df], axis=1)
     
     #save result to file
-    new_df.to_csv('df_sentiment_analysis.csv', index=False)
+    new_df.to_csv('features/sentiment_analysis/df_sentiment_analysis.csv', index=False)
     
 df_sentiment_analysis = sentiment_analysis(mcd_file)
